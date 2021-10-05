cb416cef1a49bf23c4107add9037c06a9c05beae
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2021-10-05 17:23:18 +0900

    로그인 기능 구현
    아이디와 비밀번호가 맞지 않는다면
    1.아이디 비밀번호가 틀리다고 경고 글자를 inputbox위에 넣어줌
    2.inputbox가 빨간색으로 바뀜

--- shoppingmall/sign-in/sign-in.js
+++ shoppingmall/sign-in/sign-in.js
@@ -1,39 +1,80 @@
+const $idError = document.querySelector('.id-error');
+const $passwordError = document.querySelector('.password-error');
+const $inputId = document.querySelector('.input-id');
+const $inputPassword = document.querySelector('.input-password');
 
 function signUp() {
 }
 
 function validateUserInput(id,password) {
-  const emailValidation = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+  const checkEmailValidation = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
   // 숫자, 특문 각 1회 이상, 영문은 2개 이상 사용하여 8자리 이상 입력
-  const passwordValidation = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+  const checkPasswordValidation = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
   // 8~16자리 숫자 영문 특수문자 포함
-  if (!emailValidation.test(id)) {
-    showError('아이디 혹은 비밀번호가 틀립니다');
+  let idValue = '';
+  let passwordValue = '';
+  if (!checkEmailValidation.test(id)) {
+    idValue = showError(id);
+    return false
   }
 
-  if (!passwordValidation.test(password)) {
-    showError('아이디 혹은 비밀번호가 틀립니다');
+  if (!checkPasswordValidation.test(password)) {
+    passwordValue = showError(password);
+    return false
+  }
+
+  return showError(id, password);
+
+}
+
+function showError(id, password) {
+
+  console.log($inputId);
+  if (id !== 'name2960@naver.com') {
+    $idError.textContent = '아이디가 틀립니다';
+    $idError.style.display = "block";
+    $inputId.style.outline = "solid 1px red";
+    return false;
+  } else {
+    $idError.style.display = "none";
+    $inputId.style.outline = "0px";
+  }
+
+  if (password !== 'name2960') {
+    $passwordError.textContent = '비밀번호가 틀립니다';
+    $passwordError.style.display = "block";
+    $inputPassword.style.outline = "solid 1px red";
+    return false;
+  } else {
+    $passwordError.style.display ="none";
+    $inputPassword.style.outline = "0px";
   }
 
   return true;
 }
 
-function showError(message) {
-  console.error(message);
+function hideWarn(id, password) {
+
+  $idError.style.display = 'none';
+  $passwordError.style.display = 'none';
+  return true;
+
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
+
   const $signUp = document.querySelector('.button-sign-up');
   const $signIn = document.querySelector('.form-sign-in');
-
   $signUp.addEventListener('click', function(event) {
+
     event.preventDefault();
     signUp();
   })
 
   $signIn.addEventListener('submit', function(event) {
-    event.preventDefault();
 
+    event.preventDefault();
     const signInData = new FormData(event.target);
     const body = {
       id: signInData.get('input-id'),
@@ -43,8 +84,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     if (validateUserInput(body.id, body.password)) {
 
     }
-
-    console.log(body);
+    
+    return false;
   });
 });
 
