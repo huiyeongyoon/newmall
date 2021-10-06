f42169dd6e2982327dfcd0baf9daa11354355307
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2021-10-06 19:54:48 +0900

    modal 기능 구현
    modal 기능 구현 esc 또는 마우스로 닫기 버튼 화면 클릭시 모달창 종료

--- shoppingmall/sign-in/sign-in.js
+++ shoppingmall/sign-in/sign-in.js
@@ -1,76 +1,93 @@
-const $idError = document.querySelector('.id-error');
-const $passwordError = document.querySelector('.password-error');
 const $inputId = document.querySelector('.input-id');
 const $inputPassword = document.querySelector('.input-password');
-
+const $modalWrapper = document.querySelector('.modal-wrapper');
+const $buttonClose = document.querySelector('.button-close');
 function signUp() {
 }
+
 
 function validateUserInput(id,password) {
   const checkEmailValidation = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
   // 숫자, 특문 각 1회 이상, 영문은 2개 이상 사용하여 8자리 이상 입력
   const checkPasswordValidation = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
   // 8~16자리 숫자 영문 특수문자 포함
-  let idValue = '';
-  let passwordValue = '';
+
   let flag = false;
   if (!checkEmailValidation.test(id)) {
-    idValue = showError(id);
+    validateUserInfo(id);
+    return false;
   }
 
   if (!checkPasswordValidation.test(password)) {
-    passwordValue = showError(password);
+    validateUserInfo(password);
+    return false;
   }
-
-  if (showError(id, password)) {
-
-  }
+  validateUserInfo(id, password);
 
   return true;
 }
 
-function showError(id, password) {
+function showIdError(id) {
+  $inputId.placeholder = '아이디가 틀립니다';
+  $inputId.placeholder.color = 'red';
+  $inputId.style.outline = 'solid 1px red';
+  $inputId.focus();
+  $modalWrapper.classList.add('modal-wrapper-active');
+  return false;
+}
 
-  console.log($inputId);
+function showPasswordError(password) {
+  $inputPassword.placeholder = '비밀번호가 틀립니다';
+  $inputPassword.placeholder.color = 'red';
+  $inputPassword.style.outline = 'solid 1px red';
+  $inputPassword.focus();
+
+  return true;
+}
+
+function validateUserInfo(id, password) {
+
+  console.log(id)
+  console.log(password)
+  // 왜 아이디만 적으면 공백으로 나올까?
+
   if (id !== 'name2960@naver.com') {
-    $idError.textContent = '아이디가 틀립니다';
-    $idError.style.display = "block";
-    $inputId.style.outline = "solid 1px red";
-    $inputId.focus();
+    showIdError();
     return false;
   } else {
-    $idError.style.display = "none";
-    $inputId.style.outline = "0px";
+    $inputId.style.outline = '0px';
   }
 
-  if (password !== 'name2960') {
-    $passwordError.textContent = '비밀번호가 틀립니다';
-    $passwordError.style.display = "block";
-    $inputPassword.style.outline = "solid 1px red";
-    $inputPassword.focus();
+  if (password !== 'name@960') {
+    showPasswordError();
     return false;
   } else {
-    $passwordError.style.display ="none";
-    $inputPassword.style.outline = "0px";
+    $inputPassword.style.outline = '0px';
   }
 
   return true;
 }
 
-function hideWarn(id, password) {
-
-  $idError.style.display = 'none';
-  $passwordError.style.display = 'none';
-  return true;
-
-}
-
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', function(event) {
   const $signUp = document.querySelector('.button-sign-up');
   const $signIn = document.querySelector('.form-sign-in');
   $signUp.addEventListener('click', function(event) {
     event.preventDefault();
     signUp();
+  })
+
+  $buttonClose.addEventListener('click', function(event) {
+    $modalWrapper.classList.remove('modal-wrapper-active');
+  })
+
+  window.addEventListener('keyup', function(event) {
+    if(event.key === "Escape") {
+      $modalWrapper.classList.remove('modal-wrapper-active');
+    }
+  })
+
+  $modalWrapper.addEventListener('click', function(event) {
+    $modalWrapper.classList.remove('modal-wrapper-active');
   })
 
   $signIn.addEventListener('submit', function(event) {
