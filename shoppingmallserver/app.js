e3c81c589dc1a6a8db6cc228d22477ed32936199
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2021-10-11 15:05:58 +0900

    express api 셋팅 완료

---
+++ shoppingmallserver/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const signInRouter = require('./routes/signIn');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/signIn', signInRouter);
+
+module.exports = app;
