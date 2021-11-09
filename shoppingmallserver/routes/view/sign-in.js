80eae721d6ab8b8133b741186b774c0e282279b4
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2021-11-09 23:56:15 +0900

    라우터 수정

---
+++ shoppingmallserver/routes/view/sign-in.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+// 1파일 불러오기
+const controller = require('../controller/sign-in');
+
+router.post('/', controller.signIn);
+
+module.exports = router;
