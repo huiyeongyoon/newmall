8bf7ea0a6a3da81416dd9b9094a262f112496df1
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2021-11-12 18:49:42 +0900

    캐러셀 추가

--- shoppingmallserver/public/productdetail/productDetail.js
+++ shoppingmallserver/public/productdetail/productDetail.js
@@ -1,9 +1,83 @@
-const $listWrapper = document.querySelector('.list-wrapper');
-$listWrapper.addEventListener('mouseover',function(event) {
-  const $active = document.querySelector('#active');
-  $active.removeAttribute('id');
-})
 
-$listWrapper.addEventListener('mouseout',function(event) {
-  $listWrapper.childNodes[1].children[2].childNodes[0].setAttribute('id', 'active');
-})
+window.addEventListener('DOMContentLoaded', function(event) {
+  const $listWrapper = document.querySelector('.list-wrapper');
+
+  const $slides = document.querySelector('.slides');
+  const slide = document.querySelectorAll('.slides li');
+  let currentIndex = 0;
+  const slideCount = slide.length;
+  const slideWidth = 1000;
+  const slideMargin = 60;
+  const $prev = document.querySelector('.prev');
+  const $next = document.querySelector('.next');
+
+  $listWrapper.addEventListener('mouseover',function(event) {
+    const $active = document.querySelector('#active');
+    $active.removeAttribute('id');
+  })
+
+  $listWrapper.addEventListener('mouseout',function(event) {
+    $listWrapper.childNodes[1].children[2].childNodes[0].setAttribute('id', 'active');
+  })
+
+
+  makeClone();
+
+  function makeClone() {
+    for (let i = 0; i < slideCount; i++) {
+      const cloneSlide = slide[i].cloneNode(true);
+      cloneSlide.classList.add('clone');
+      $slides.appendChild(cloneSlide);
+    }
+    for (let i = slideCount - 1; i >= 0; i--) {
+      const cloneSlide = slide[i].cloneNode(true);
+      cloneSlide.classList.add('clone');
+      $slides.prepend(cloneSlide);
+    }
+    updateWidth();
+    setInitialPosition();
+    setTimeout(function(){
+      $slides.classList.add('animated');
+    }, 100);
+
+  }
+
+  function updateWidth() {
+    const currentSlides = document.querySelectorAll('.slides li');
+    const newSlideCount = currentSlides.length;
+
+    const newWidth = (slideWidth + slideMargin) * newSlideCount - slideMargin + 'px';
+    $slides.style.width = newWidth;
+
+  }
+  function setInitialPosition() {
+    const initialTranslateValue = -(slideWidth + slideMargin) * slideCount;
+    $slides.style.transform = 'translateX(' + initialTranslateValue + 'px)';
+  }
+
+  $next.addEventListener('click', function() {
+    moveSlide(currentIndex + 1);
+  });
+  $prev.addEventListener('click', function() {
+    moveSlide(currentIndex - 1);
+  });
+
+  function moveSlide(num) {
+    $slides.style.left = -num * (slideWidth + slideMargin) + 'px';
+    currentIndex = num;
+    console.log(currentIndex, slideCount);
+
+    if(currentIndex === slideCount || currentIndex === -slideCount) {
+      setTimeout(function() {
+        $slides.classList.remove('animated');
+        $slides.style.left = '0px';
+        currentIndex = 0;
+      }, 500);
+    }
+    setTimeout(function() {
+      $slides.classList.add('animated');
+    },600);
+
+  }
+});
+
