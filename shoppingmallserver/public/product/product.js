d449f9b08e4a6682540270a23de81e5119bca775
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2021-11-14 20:14:08 +0900

    각 페이지 css 수정

--- shoppingmallserver/public/product/product.js
+++ shoppingmallserver/public/product/product.js
@@ -1,4 +1,7 @@
 const $listWrapper = document.querySelector('.list-wrapper');
+const $sort = document.querySelector('.sort');
+const $image = document.querySelectorAll('.image');
+
 $listWrapper.addEventListener('mouseover',function(event) {
   const $active = document.querySelector('#active');
   $active.removeAttribute('id');
@@ -7,3 +10,15 @@ $listWrapper.addEventListener('mouseover',function(event) {
 $listWrapper.addEventListener('mouseout',function(event) {
   $listWrapper.childNodes[1].children[1].childNodes[0].setAttribute('id', 'active');
 })
+
+window.onload = () => {
+  $sort.addEventListener('click', function(event){
+    event.preventDefault();
+    if(event.target.classList.contains('sort-name')) {
+      $sort.querySelector('.active').classList.remove('active');
+      event.target.classList.add('active');
+      let eventName = event.target.getAttribute('data-name');
+      console.log(eventName);
+    }
+  })
+}
