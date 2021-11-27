47f3894e68fced2e4fbba09c68b802ccf0248b77
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2021-11-27 12:14:05 +0900

    암호 비밀번호 객체화

--- shoppingmallserver/routes/controller/sign-in.js
+++ shoppingmallserver/routes/controller/sign-in.js
@@ -1,19 +1,24 @@
 
+const TempLoginInfo = {
+  userId: 1,
+  id: 'test@test.com',
+  password: 'test!234',
+};
+
 const signIn = (req, res) => {
   const { body: { id, password } } = req;
 
-  if (id === 'test@test.com' && password === 'test!234') {
-    return res.json({
-      success: true,
-      signInData: req.session.signInData
-    });
-  } else {
-    return res.json({
-      success: false
-    });
+  if (TempLoginInfo.id === id && TempLoginInfo.password === password) {
+    req.session.userId = TempLoginInfo.userId;
+    res.json({ ok: true });
+    return;
   }
-}
+  res.json({ ok: false });
+};
+
 
 module.exports = {
   signIn
 };
+
+
